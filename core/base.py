5d64d251419e74cce03aa8fdf1e8533580c8b80c
papadanku <pauldang2@gmail.com>
2022-11-10 22:56:04 -0700

    Complete 2.1

---
+++ core/base.py
@@ -0,0 +1,67 @@
+
+# Sets base program for other files to inherit
+
+import pygame
+import sys
+
+class Base(object):
+
+    # initialize the object's display attributes
+    def __init__(self, screenSize = [512, 512]):
+
+        # initialize all pygame modules
+        pygame.init()
+
+        # indicate rendering details
+        displayFlags = pygame.DOUBLEBUF | pygame.OPENGL
+
+        # initialize buffers to perform antialiasing
+        pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLEBUFFERS, 1)
+        pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLESAMPLES, 4)
+
+        # use a core OpenGL profile for cross-platform compatibility
+        pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
+
+        # create and display the window
+        self.screen = pygame.display.set_mode(screenSize, displayFlags)
+
+        # set the text that appears in the title bar of the window
+        pygame.display.set_caption("Graphics Window")
+
+        # determine if main loop is active
+        self.running = True
+
+        # manage time-related data and operations
+        self.clock = pygame.time.Clock()
+    
+    # implement by extending class
+    def initialize(self):
+        pass
+
+    # implement by extending class
+    def update(self):
+        pass
+
+    def run(self):
+
+        ## startup ##
+        self.initialize()
+
+        ## main loop ##
+        while self.running:
+
+            ## process input ##
+
+            ## update ##
+            self.update()
+
+            ## render ##
+            # display image on screen
+            pygame.display.flip()
+
+            # pause if necessary to achive 60fps
+            self.clock.tick(60)
+        
+        ## shutdown ##
+        pygame.quit()
+        sys.exit()
---
+++ core/input.py
@@ -0,0 +1,18 @@
+
+import pygame
+
+class Input(object):
+
+    def __init__(self):
+        # has the user quit the application?
+        self.quit = False
+
+    def update(self):
+        """
+        iterate all over user input events (such as keyboard or mouse)
+        that occured since the last time events were checked
+        """
+        for event in pygame.event.get():
+            # quit event occurs by clicking button to close window
+            if event.type == pygame.quit:
+                self.quit = True
